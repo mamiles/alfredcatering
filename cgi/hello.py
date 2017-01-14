eb36a3e2edc55d2557be8deba58a3ef958a00fad
Marvin Miles <mamiles@cisco.com>
2017-01-14 17:18:15 -0600

    Add first CGI pgm

--- cgi/hello.py
+++ cgi/hello.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+import cgi
 
 print("Content-type:text/html\r\n\r\n")
 print('<html>')
 print('<head><title>My First CGI Program</title></head>')
 print('<body>')
-print('<p> It works!!! </p>')
-for i in range(5):
-    print('<h1>Hello World! ' + i + '</h2>')
+print('<h1>Hello Program!</h1>')
+form = cgi.FieldStorage()
+if form.getvalue("name"):
+    name = form.getvalue("name")
+    print('<h1>Hello ' + name + '! Thanks for using my script!</h1><br />')
+if form.getvalue("happy"):
+    print("<p> Yay! I'm happy too! </p>")
+if form.getvalue("sad"):
+    print("<p> Oh no! Why are you sad? </p>")
+
+print('<form method="post" action="hello.py">')
+print('<p>Name: <input type="text" name="name"/></p>')
+print('<input type="checkbox" name="happy" /> Happy')
+print('<input type="checkbox" name="sad" /> Sad')
+print('<input type="submit" value="Submit" />')
+print('</form>')
 print('</body>')
 print('</html>')
