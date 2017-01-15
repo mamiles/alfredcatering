e8dfb6a3c95e4a18cab5f5b9d6b588f176b82c1d
Marvin Miles <mamiles@cisco.com>
2017-01-15 16:39:37 -0600

    Try calling contact.py frist time.

--- cgi/contact.py
+++ cgi/contact.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import cgi
 import cgitb
+import os
 
 cgitb.enable()
 
@@ -15,6 +16,9 @@ firstName = form.getfirst("firstName", "").upper()
 
 print('<h1>Hello ' + firstName + '! Thanks for using my script!</h1><br />')
 
+print("<font size=+1>Environment</font><\br>")
+for param in os.environ.keys():
+    print("<b>%20s</b>: %s<\br>" % (param, os.environ[param]))
 
 print('</body>')
 print('</html>')
