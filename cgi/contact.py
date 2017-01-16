c58cc178e977d7d87e469b514a1bc5ea716c0a9b
Marvin Miles <mamiles@cisco.com>
2017-01-15 20:37:03 -0600

    Put in ReCaptcha

--- cgi/contact.py
+++ cgi/contact.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+
 import cgi
 import cgitb
+from reCaptcha import ReCaptcha
 
 cgitb.enable()
 cgitb.enable(display=0, logdir="/local/sites/alfredcatering.com/log/cgi.log")
+
+captcha = reCaptcha(secretKey='6Lf_2BEUAAAAAFp8AUaJGKjvccufmlXok_Ouu0SL')
 
 print("Content-type:text/html\r\n\r\n")
 print('<html>')
@@ -15,8 +19,9 @@ form = cgi.FieldStorage()
 firstName = form.getfirst("firstName", "")
 lastName = form.getfirst("lastName", "")
 recaptcha = form.getfirst('g-recaptcha-response', '')
+value = captcha.is_succes(recaptcha)
 
-print('<h1>Hello ' + firstName + ' ' + recaptcha + '! Thanks for using my script!</h1><br />')
+print('<h1>Hello ' + firstName + ' ' + value + '! Thanks for using my script!</h1><br />')
 
 
 print('</body>')
---
+++ cgi/reCaptcha.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+
+class ReCaptcha(object):
+    def __init__(self, secret_key):
+
+        self.secret_key = secret_key
+        self.url = 'https://www.google.com/recaptcha/api/siteverify'
+        self.data = {}
+
+    def __callApi(self, response):
+        try:
+            headers = {'User-Agent': 'DebuguearApi-Browser', }
+            payload = {'secret': self.secret_key, 'response': response}
+            r = requests.request(method='POST', url=self.url, headers=headers, data=payload)
+            return r.json()
+
+        except Exception:
+            print(sys.exc_info())
+            return False
+
+    def is_success(self, response):
+        self.data = self.__callApi(response)
+        if self.data:
+            try:
+                return self.data['success']
+            except:
+                return None
+        else:
+            return None
+
+    def get_error_codes(self):
+        """
+        missing-input-secret	The secret parameter is missing.
+        invalid-input-secret	The secret parameter is invalid or malformed.
+        missing-input-response	The response parameter is missing.
+        invalid-input-response	The response parameter is invalid or malformed.
+        """
+        try:
+            return self.data['error-codes']
+        except:
+            return None
+
+    def get_hostname(self):
+        try:
+            return self.data['hostname']
+        except:
+            return None
+
+    def get_challenge_ts(self):
+        #return time stamp
+        try:
+            return self.data['challenge_ts']
+        except:
+            return None
