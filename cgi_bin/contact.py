0410d5e644d5187efea2151f85e5e36c216a20ed
Marvin Miles <mamiles@cisco.com>
2017-01-15 21:18:29 -0600

    Put in ReCaptcha

--- cgi_bin/contact.py
+++ cgi_bin/contact.py
@@ -4,7 +4,7 @@ import cgi
 import cgitb
 import sys
 import os
-# sys.path.insert(0, "/local/sites/alfredcatering.com/htdocs/cgi_bin")
+sys.path.insert(0, "/local/sites/alfredcatering.com/htdocs")
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
